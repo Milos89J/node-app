1772422ffaa93662e5fb8793743c6f1549cada31
Milos Jeremic <Milos.Jeremic@bdkadvokati.com>
2022-11-24 11:19:22 +0100

    async

---
+++ 10-async.js
@@ -0,0 +1,26 @@
+const { readFile, writeFile } = require('fs')
+
+readFile('./content/first.txt', 'utf8', (err, result) =>{
+    if (err) {
+        console.log(err)
+        return
+    }
+    const first = result;
+    readFile('./content/first.txt', 'utf8', (err, result) =>{
+        if (err) {
+            console.log(err)
+            return
+        }
+        const second = result
+        writeFile(
+            './content/write.txt', `w : ${first}, ${second}`
+            ,(err, result)=> {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                console.log(result)
+            }
+        )
+    })
+})
